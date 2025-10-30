2a9b242b1f0f11744d8dcd1869dcecfaf09240d6
vishal chauhan <vishalchauhan2109@gmail.com>
2025-10-30 19:07:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Toggle Menu for Mobile
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
